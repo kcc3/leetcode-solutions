2df49aea05241ba44a2aed558c44a187a76a2c36
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2020-06-16 23:37:54 -0700

    Two Sum
    A more optimized solution for two sum - this iterates through the list once and hashes the difference so you can check for it as you iterate through the rest of the list

--- problems/python/two_sum.py
+++ problems/python/two_sum.py
@@ -10,10 +10,12 @@ def twoSum(nums, target):
     Returns:
         list: list containing the two indices of the two numbers that add up to the target
     """
+    hash = {}
     for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+        if nums[i] not in hash:
+            hash[target - nums[i]] = i
+        else:
+            return [hash[nums[i]], i]
 
 
 if __name__ == "__main__":
