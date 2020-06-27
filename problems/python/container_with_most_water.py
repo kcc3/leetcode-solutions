d978492e66920edf0bd5b18e940b6b8b9a726bcd
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2020-06-27 16:39:09 -0700

    Update container_with_most_water.py
    Optimized solve - we use the two pointer approach where we start from the front and back of the list, and iterate towards the center, stepping based on the lower height so we're always looking for the maximized area when we are stepping

--- problems/python/container_with_most_water.py
+++ problems/python/container_with_most_water.py
@@ -1,9 +1,27 @@
 def max_area(height: list) -> int:
+    """Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical
+    lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with
+    x-axis forms a container, such that the container contains the most water.
+
+    Solve:
+        We use the two pointer approach where we start from the front and back of the list, and iterate towards the
+        center, stepping based on the lower height so we're always looking for the maximized area when we are stepping
+
+    Args:
+        height (list): Array of heights
+
+    Returns:
+        int: The maximum area container we can create
+    """
     area = 0
-    for i in range(len(height)):
-        for j in range(i+1, len(height)):
-            cur_area = min(height[i], height[j]) * (j - i)
-            area = max(cur_area, area)
+    front = 0
+    back = len(height) - 1
+    while front != back:
+        area = max(area, min(height[front], height[back]) * (back - front))
+        if height[front] > height[back]:
+            back -= 1
+        else:
+            front += 1
     return area
 
 
